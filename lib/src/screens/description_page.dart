e47d0b40cf0ea8bea8c5ff3d6750ff78f8447c65
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2024-06-30 19:19:48 +0300

    created the product description page

--- lib/src/screens/description_page.dart
+++ lib/src/screens/description_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pet_food/utils/colors.dart';
 
 import '../../utils/specials_banner_images.dart';
 class DescriptionPage extends StatelessWidget {
@@ -8,6 +9,56 @@ class DescriptionPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Container(
+        height: MediaQuery.of(context).size.height*0.09,
+        decoration: BoxDecoration(
+          color: Color(0xFFF2F8FC),
+          borderRadius: BorderRadius.circular(12)
+        ),
+        child: Row(
+          children: [
+            Expanded(
+                flex: 4,
+                child: Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Total Price",style: TextStyle(fontSize: 18,color: Colors.grey,fontWeight: FontWeight.w600),),
+                  Text("400 Ksh",style: TextStyle(fontSize: 25,fontWeight: FontWeight.w800,color: Colors.black,),)
+                ],
+              ),
+            )),
+            Expanded(
+                flex: 5,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0,vertical: 8),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: primayColor,
+                      borderRadius: BorderRadius.circular(25)
+                    ),
+                    child: Center(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.shopping_cart,color: Colors.white,),
+                          const SizedBox(width: 5,),
+                          Text(
+                            "Add to Cart",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                              ),
+                ))
+          ],
+        ),
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(5.0),
@@ -101,12 +152,87 @@ class DescriptionPage extends StatelessWidget {
                 fontWeight: FontWeight.w700,
                 fontSize: 20
               ),),
+              const SizedBox(height: 5,),
+              Container(
+                padding: EdgeInsets.all(8),
+                height: 100,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Seller",style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w700,
+                    ),),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Container(
+                              height: 60,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle,
+                                color: Colors.grey
+                              ),
+                            ),
+                            const SizedBox(width: 10,),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text("John Doe",style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w700,
+                                  ),),
+                                  Text("Manager",style: TextStyle(
+                                    fontSize: 15,
+                                    fontWeight: FontWeight.w500,
+                                    color: Colors.grey
+                                  ),)
+                                ]
+                            ),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            userSectionIcons(Icons.messenger_outline),
+                            const SizedBox(width: 10,),
+                            userSectionIcons(Icons.phone),
+                          ],
+                        )
+                      ],
+                    ),
+                    Text("Select Sizes"),
+                    ListView.builder(
+                        itemCount: 5,
+                        itemBuilder: (context,index){
+                          return Container(
 
+                          );
+                    })
+                  ],
+                ),
+              )
             ],
           ),
         ),
       ),
     );
+  }
+
+  Container userSectionIcons(IconData icon) {
+    return Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Color(0xFFF4F1F8),
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Icon(icon,color: primayColor,),
+                            ),
+                          );
   }
 
   Container descIcons(IconData icon) {
