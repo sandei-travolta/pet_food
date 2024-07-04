b3c2970dff16f78a4099e65c6784085514748e7d
Travolta Sandei <67733769+sandei-travolta@users.noreply.github.com>
2024-07-04 13:00:16 +0300

    created the product description page

--- lib/src/screens/description_page.dart
+++ lib/src/screens/description_page.dart
@@ -62,160 +62,183 @@ class DescriptionPage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(5.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Stack(
-                children: [
-                  Container(
-                    color: Colors.grey.withOpacity(0.5),
-                    height: MediaQuery.of(context).size.height*0.5,
-                    child: Column(
-                      children: [
-                        Expanded(flex: 3,child: Container(
-                          padding: EdgeInsets.all(35),
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Image.asset(bannerImage1,fit: BoxFit.fill,)),
-                        )),
-                        Expanded(flex:1,child: Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 15.0,horizontal: 20),
-                          child: Container(
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: ListView.builder(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: 6,
-                              itemBuilder: (context,index){
-                            return Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                  color: Colors.grey.withOpacity(0.6),
-                                  borderRadius: BorderRadius.circular(12)
-                                ),
-                                width: 60,
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(12),
-                                    child: Image.asset(bannerImage1,fit: BoxFit.fill,),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Stack(
+                  children: [
+                    Container(
+                      color: Colors.grey.withOpacity(0.5),
+                      height: MediaQuery.of(context).size.height*0.5,
+                      child: Column(
+                        children: [
+                          Expanded(flex: 3,child: Container(
+                            padding: EdgeInsets.all(35),
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(12),
+                              child: Image.asset(bannerImage1,fit: BoxFit.fill,)),
+                          )),
+                          Expanded(flex:1,child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 15.0,horizontal: 20),
+                            child: Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                itemCount: 6,
+                                itemBuilder: (context,index){
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey.withOpacity(0.6),
+                                    borderRadius: BorderRadius.circular(12)
+                                  ),
+                                  width: 60,
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(12),
+                                      child: Image.asset(bannerImage1,fit: BoxFit.fill,),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            );
-                          }),
-                          ),
-                        )),
-                      ],
-                    ),
-                  ),
-                  Positioned(child: Container(
-                      padding: EdgeInsets.all(10),
-                      width: double.maxFinite,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          descIcons(Icons.arrow_back),
-                          Row(
-                            children: [
-                            descIcons(Icons.favorite_border_rounded),
-                            const SizedBox(width: 5,),
-                              descIcons(Icons.share)
-                          ],)
+                              );
+                            }),
+                            ),
+                          )),
                         ],
-                      )))
-                ],
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text("Dog Food",style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.grey
-                    ),),
-                    Row(
-                      children: [
-                        Icon(Icons.star,color: Colors.yellow,),
-                        Text("4.9")
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Text("NutriNosh Dog Dry Food",style: TextStyle(
-                fontWeight: FontWeight.w700,
-                fontSize: 20
-              ),),
-              const SizedBox(height: 5,),
-              Container(
-                padding: EdgeInsets.all(8),
-                height: 100,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text("Seller",style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
-                    ),),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Container(
-                              height: 60,
-                              width: 60,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.grey
-                              ),
-                            ),
-                            const SizedBox(width: 10,),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text("John Doe",style: TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w700,
-                                  ),),
-                                  Text("Manager",style: TextStyle(
-                                    fontSize: 15,
-                                    fontWeight: FontWeight.w500,
-                                    color: Colors.grey
-                                  ),)
-                                ]
-                            ),
-                          ],
-                        ),
-                        Row(
+                      ),
+                    ),
+                    Positioned(child: Container(
+                        padding: EdgeInsets.all(10),
+                        width: double.maxFinite,
+                        child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            userSectionIcons(Icons.messenger_outline),
-                            const SizedBox(width: 10,),
-                            userSectionIcons(Icons.phone),
+                            descIcons(Icons.arrow_back),
+                            Row(
+                              children: [
+                              descIcons(Icons.favorite_border_rounded),
+                              const SizedBox(width: 5,),
+                                descIcons(Icons.share)
+                            ],)
                           ],
-                        )
-                      ],
-                    ),
-                    Text("Select Sizes"),
-                    ListView.builder(
-                        itemCount: 5,
-                        itemBuilder: (context,index){
-                          return Container(
-
-                          );
-                    })
+                        )))
                   ],
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("Dog Food",style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                        color: Colors.grey
+                      ),),
+                      Row(
+                        children: [
+                          Icon(Icons.star,color: Colors.yellow,),
+                          Text("4.9")
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Text("NutriNosh Dog Dry Food",style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: 20
+                ),),
+                const SizedBox(height: 5,),
+                Container(
+                  padding: EdgeInsets.all(8),
+                  height: 100,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Seller",style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.w700,
+                      ),),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Container(
+                                height: 60,
+                                width: 60,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.grey
+                                ),
+                              ),
+                              const SizedBox(width: 10,),
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("John Doe",style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w700,
+                                    ),),
+                                    Text("Manager",style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.w500,
+                                      color: Colors.grey
+                                    ),)
+                                  ]
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              userSectionIcons(Icons.messenger_outline),
+                              const SizedBox(width: 10,),
+                              userSectionIcons(Icons.phone),
+                            ],
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Text("Select Size",style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600
+                ),),
+                Container(
+                  height: 50,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 5,
+                      itemBuilder: (context,index){
+                        return Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(12),
+                              color: Color(0xFFF2F8FC),
+                              border: Border.all(
+                                width: 2,
+                                color: Colors.black12
+                              )
+                            ),
+                            padding: EdgeInsets.symmetric(vertical: 3,horizontal: 10),
+                            child: Center(
+                              child: Text("${index+1} Kg"),
+                            ),
+                          ),
+                        );
+                      }),
+                )
+              ],
+            ),
           ),
         ),
       ),
